1712ea6ca94e16b9fd5b0b541e5d768e5cd8d0f8
Nicholas Minor <nrminor@wisc.edu>
2023-09-25 13:36:26 -0500

    organizing the core rust components of the project

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
+pub mod lib {
 
+    // Expose the core LGC commands
+    pub mod lgc_commands;
+
+    // Available languages
+    pub mod golang;
+    pub mod julia;
+    pub mod rlang;
+    pub mod python;
+    pub mod mojo;
+    pub mod rust;
+    pub mod nextflow;
+
+}
