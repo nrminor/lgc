fda5b924ea60d48c315cf5d2c164d4fa12b67ea8
Nicholas Minor <nrminor@wisc.edu>
2023-10-01 15:01:33 -0500

    let the abstraction of complexity begin

--- src/lib/python.rs
+++ src/lib/python.rs
@@ -1,1 +1,75 @@
+use std::env;
+use std::fs;
+use std::io::{self, Write};
+use std::process::Command;
 
+fn copy_template(project_name: &String) -> std::io::Result<()> {
+    // Get the current executable's path
+    let exe_path = env::current_exe()?;
+    let root_dir = exe_path
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap();
+
+    // Form the path to the source file relative to the executable
+    let src_file = root_dir.join("templates/Python/template.py");
+
+    // Get the current working directory
+    let dest_dir = env::current_dir()?;
+
+    // Form the path to the destination file
+    let dest_file = dest_dir.join(format!("{}/{}/template.py", project_name, project_name));
+
+    // Copy the file
+    fs::copy(src_file, dest_file)?;
+
+    Ok(())
+}
+
+pub fn build_env(project_name: &String) -> std::io::Result<()> {
+    let check_poetry = Command::new("poetry").arg("--version").output();
+
+    match check_poetry {
+        Ok(_) => {
+            let _ = Command::new("poetry")
+                .arg("new")
+                .arg(&project_name)
+                .output();
+            copy_template(&project_name)?;
+        }
+        Err(_) => {
+            loop {
+                // begin user prompt
+                println!("Poetry installation not found. Would you like to install it? (y/n)");
+
+                // Print the prompt
+                print!(">> ");
+                io::stdout().flush()?;
+
+                // Read a line of input
+                let mut input = String::new();
+                io::stdin().read_line(&mut input)?;
+
+                // Remove the newline character
+                let input = input.trim();
+
+                // Handle the input
+                if input.to_lowercase() == "y" || input.to_lowercase() == "yes" {
+                    let _ = Command::new("pip").arg("install").arg("poetry").output();
+
+                    let _ = Command::new("poetry")
+                        .arg("new")
+                        .arg(&project_name)
+                        .output();
+                    copy_template(&project_name)?;
+                } else {
+                    panic!("Python environment cannot be set up without a Poetry installation.")
+                }
+            }
+        }
+    }
+    Ok(())
+}
