06eb07d9e5984d9e2f5d9b347e1ab64ee1114e7f
Nicholas Minor <nrminor@wisc.edu>
2023-10-03 15:57:12 -0500

    small readability tweak

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,14 @@ fn main() {
     // very early stages of sussing out code in main, starting with python
     match language {
         Language::Python => {
-            python::build_env(&project_name).expect("Failed to build Python environment")
+            python::build_env(&project_name).expect("Failed to build Python environment");
         }
         Language::Julia => {
-            julia::build_env(&project_name).expect("Failed to build Julia environment")
+            julia::build_env(&project_name).expect("Failed to build Julia environment");
         }
-        Language::Go => golang::build_env(&project_name).expect("Failed to build Go environment"),
+        Language::Go => {
+            golang::build_env(&project_name).expect("Failed to build Go environment");
+        }
         _ => panic!("Only Python, Julia, and Go are supported at this time."),
     }
 }
